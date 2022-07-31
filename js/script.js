905465a77c023e5e0673cce4099f7d100fadd8a0
Valentyn <valentunpron@gmail.com>
2022-07-31 16:51:58 +0300

    complete header +adaptivve

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-'use strict';
-
-const burger = document.querySelector('.header__burger'),
-	  burgerBody = document.querySelector('.menu__body'),
-	  burgerList = document.querySelector('.menu__list'),
-	  burgerItem = burgerList.querySelector('.menu__item');
-
-burger.addEventListener('click', (event) => {
-	burgerBody.classList.toggle('menu__body')
-})
+$(document).ready( () => {
+	$('.header__burger').click((event) => {
+		$('.header__burger, .header__menu').toggleClass('active');
+		$('body').toggleClass('lock')
+	})
+});
