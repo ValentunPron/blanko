36c85d34610da4bd3a79922442620e5d094e2893
Valentyn <valentunpron@gmail.com>
2022-08-01 10:24:29 +0300

    slider

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ $(document).ready( () => {
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock')
 	})
+	$('.slide').slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear'
+	});
 });
