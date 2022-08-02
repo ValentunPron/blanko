d06668a7d0d83a42fa682ecaaed5ba417046a3ea
Valentyn <valentunpron@gmail.com>
2022-08-02 12:05:34 +0300

    Complete work this slider html and css

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
 $(document).ready( () => {
+	// burger
 	$('.header__burger').click((event) => {
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock')
 	})
-	$('.slide').slick({
+
+	// slider
+	$('.slider__body').slick({
 		dots: true,
+		appendDots:$('.slider__dots'),
 		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 15000,
+		arrows: false,
 		speed: 500,
 		fade: true,
-		cssEase: 'linear'
+		cssEase: 'linear',
 	});
+
+	var activeClass = 'slick-active',
+    ariaAttribute = 'aria-hidden';
+	$( '.slider__body' )
+	.on( 'init', function() {
+		$( '.slick-dots li:first-of-type' ).addClass( activeClass ).attr( ariaAttribute, false );
+	} )
+	.on( 'afterChange', function( event, slick, currentSlide ) {
+		var $dots = $( '.slick-dots' );
+		$( 'li', $dots ).removeClass( activeClass ).attr( ariaAttribute, true );
+		$dots.each( function() {
+			$( 'li', $( this ) ).eq( currentSlide ).addClass( activeClass ).attr( ariaAttribute, false );
+		} );
+	} );
+
+	$( '.slider__body' ).slick();
 });
