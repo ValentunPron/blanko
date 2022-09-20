567021ba171a2167bda468213cc045be72aeec43
Valentyn <valentunpron@gmail.com>
2022-09-20 12:24:09 +0300

    fixed bugs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-import pages from './modules/pages';
 import burger from './modules/burger';
 import slider from './modules/slider'
 
 $(document).ready( () => {
 	// slider
-	pages('http://localhost:3000/landingPages', '.landing__list');
-	pages('http://localhost:3000/otherPages', '.other__list');
 	burger('.header__burger', '.header__menu');
 	slider()
 });
