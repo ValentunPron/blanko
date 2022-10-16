f721b38778301090f7f8954790cd9419315a4b99
Valentyn <valentunpron@gmail.com>
2022-10-16 13:08:33 +0300

    scroll menu

--- js/modules/scroll.js
+++ js/modules/scroll.js
@@ -1,19 +1,46 @@
 
 function scroll() {
-	$('.menu__link').each((index, element) => {
-		console.log(element.id);
-		$(`#${element.id}`).click((e) => {
-			console.log(e.target)
-
-			$('html, body').animate({
-				scrollTop: $(`.${element.id}`).offset().top
-			}, 1000);
-			if ($('.header__burger, .header__menu').hasClass('active')) {
-				$('.header__burger, .header__menu').toggleClass('active');
-				$('body').toggleClass('lock')
-			}
-		})
-	}) 
+	$('.landing-button').click(() => {
+		$('html, body').animate({
+			scrollTop: ($('.pages__landing').offset().top) - 100
+		}, 400);
+		if ($('.header__burger, .header__menu').hasClass('active')) {
+			$('.header__burger, .header__menu').toggleClass('active');
+			$('body').toggleClass('lock')
+		}
+	});
+	$('.pages-button').click(() => {
+		$('html, body').animate({
+			scrollTop: ($('.pages__other').offset().top) - 100
+		}, 400);
+		if ($('.header__burger, .header__menu').hasClass('active')) {
+			$('.header__burger, .header__menu').toggleClass('active');
+			$('body').toggleClass('lock')
+		}
+	});
+	$('.template-button').click(() => {
+		$('html, body').animate({
+			scrollTop: ($('.template').offset().top)  - 50
+		}, 400);
+		if ($('.header__burger, .header__menu').hasClass('active')) {
+			$('.header__burger, .header__menu').toggleClass('active');
+			$('body').toggleClass('lock')
+		}
+	});
+	$('.sign-up-button').click(() => {
+		$('html, body').animate({
+			scrollTop: ($('.forms').offset().top) - 50 
+		}, 400);
+		if ($('.header__burger, .header__menu').hasClass('active')) {
+			$('.header__burger, .header__menu').toggleClass('active');
+			$('body').toggleClass('lock')
+		}
+	});
+	$('.logo__link').click(() => {
+		$('html, body').animate({
+			scrollTop: 0 
+		}, 400);
+	});
 }
 
 export default scroll;
